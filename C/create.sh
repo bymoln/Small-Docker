ff08df489edfafa116de5cfc61f096a7d4a7f445
bymoln <nospam@dev.null>
2017-10-01 19:29:09 +0200

    Messed around

---
+++ C/create.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# Create statically linked executable
+cat << EOF > HelloWorld.c
+#include <stdio.h>
+
+int main(void)
+{
+  printf("Hi World!\n");
+  return 0;
+}
+EOF
+
+gcc -static HelloWorld.c -o HelloWorld
+
+# Build docker image
+cat << EOF > Dockerfile
+FROM scratch
+LABEL version = "0.1"
+COPY HelloWorld /
+CMD ["/HelloWorld"]
+EOF
+
+# Clean up
+rm HelloWorld.c
+
+echo ""
+echo "Example to build and push to Docker Hub"
+echo "---------------------------------------"
+echo "docker build -t bymoln/helloworld-c ."
+echo "docker login"
+echo "docker push bymoln/helloworld-c"
+echo ""
+
