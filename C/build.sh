234ecd58365233654729ea034380d58a8021d4b3
bymoln <nospam@dev.null>
2017-10-01 02:19:56 +0200

    Add build script and docker info

---
+++ C/build.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Create statically linked executable
+cat << EOF > HelloWorld.c
+#include <stdio.h>
+
+int main(void)
+{
+  printf("Hi World\n");
+  return 0;
+}
+EOF
+
+gcc -static HelloWorld.c -o HelloWorld
+
+# Build docker image
+cat << EOF > Dockerfile
+FROM scratch
+COPY HelloWorld /
+CMD ["/HelloWorld"]
+EOF
+
+docker build -t helloworld-c .
+
+# Clean up
+rm HelloWorld.c HelloWorld Dockerfile
+
